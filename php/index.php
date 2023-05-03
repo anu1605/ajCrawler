2118fcbb31fe278bd93c2000d8268098ae4aafd6
Anushka Tyagi <anushka.tyagi161998@gmail.com>
2023-05-03 15:23:42 +0530

    23 may final

--- php/index.php
+++ php/index.php
@@ -5,7 +5,13 @@
 
 $array = ['agra-city',  'aligarh-city',  'almora', 'ambala', 'ambedkar-nagar', 'amethi', 'jpnagar', 'auraiya', 'faizabad', 'azamgarh', 'badaun', 'baghpat', 'bahraich', 'balia', 'balrampur', 'banda', 'barabanki', 'bareilly-city', 'basti', 'bhadohi', 'bhiwani', 'bhopal', 'bijnor', 'bilaspur', 'bulandsahar', 'chamba', 'chandauli', 'chandigarh-city', 'charkhi-dadri', 'dehradun-city', 'delhi-city', 'deoria', 'etawa', 'faridabad', 'farrukhabad', 'fatehabad', 'fatehpur', 'firozabad', 'garhwal', 'ghaziabad', 'ghazipur', 'gorakhpur-city', 'gorakhpur-dehat', 'greater-noida', 'gurgaon', 'hamirpur-dharamshala', 'hamirpur', 'hapur', 'hardoi', 'haridwar', 'hathras', 'hisar', 'jalandhar', 'jalaun', 'jammu-city', 'jounpur', 'jhajjar', 'jhansi-city', 'jind', 'kaithal', 'kangra', 'kannauj', 'kanpur-city', 'karnal', 'kasganj', 'kathua', 'kaushambi', 'kotdwar', 'kullu', 'kurukshetra', 'kushinagar', 'khiri', 'lalitpur', 'lucknow-city', 'mharajgunj', 'mainpuri', 'mandi', 'mathura', 'mau', 'meerut-city', 'mirzapur', 'mohali', 'moradabad-city', 'muzaffarnagar', 'haldwani', 'narnaul', 'noida', 'panchkula', 'panipat', 'pilibhit', 'pithoragarh', 'pratapgarh', 'allahabad-city', 'gangapar', 'raebareli', 'rajasthan', 'rampur', 'rewari', 'rishikesh', 'rishikesh', 'saharanpur-city', 'sambhal', 'santkabirnagar', 'shahjahanpur', 'shimla', 'siddharthnagar', 'sitapur', 'sonbhadra', 'sultanpur', 'udhampur', 'udhamsingh-nagar', 'una', 'unnao', 'varanasi-city', 'vikas-nagar', 'yamuna-nagar'];
 
-$date =  '2023/05/02';
+$date =  '2023/04/29';
+$time_input = strtotime($date);
+$month = date('m', $time_input);
+$year = date('y', $time_input);
+
+$currentMonth = date('m');
+$currentYear = date('y');
 
 
 
@@ -17,12 +23,15 @@ for ($i = 0; $i < count($array); $i++) {
     $content = file_get_contents($cityLink);
 
     // get city code for each city
-    $select = explode('<div class="pinch-zoom"><img id="cropbox" usemap="#enewspaper" class="news-image  map" src="https://epaperwmimg.amarujala.com/2023/05/02/', $content);
+    if ($month < $currentMonth || $year < $currentYear) {
+        $linkCorrection = "epaperimg1";
+    } else $linkCorrection = "epaperwmimg";
+    $select = explode('<div class="pinch-zoom"><img id="cropbox" usemap="#enewspaper" class="news-image  map" src="https://' . $linkCorrection . '.amarujala.com/' . $date . '/', $content);
     $code = substr($select[1], 0, 2);
 
     // loop for pages
     for ($j = 1; $j < 30; $j++) {
-        if ($j < 9)
+        if ($j <= 9)
             $pageNumber  = '0' . $j;
         else $pageNumber = $j;
         $link = 'https://epaperwmimg.amarujala.com/' . $date . '/' . $code . '/' . $pageNumber . '/hdimage.jpg';
